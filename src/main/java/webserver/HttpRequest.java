f11b1ade61879c29986da8ca6ee3aa4fca3f2201
WOWND <jjp910123@gmail.com>
2026-04-09 15:48:37 +0900

    index.html 응답
    - HttpRequest로 요청을 파싱
    - 파싱한 결과에서 경로를 꺼내서 정적 리소스에 접근하도록 구현

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,59 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class HttpRequest {
+    private final String method;
+    private final String path;
+    private final String protocol;
+    private final Map<String, String> header;
+
+    private HttpRequest(String method, String path, String protocol, Map<String, String> header) {
+        this.method = method;
+        this.path = path;
+        this.protocol = protocol;
+        this.header = header;
+    }
+
+    public static HttpRequest from(BufferedReader br) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        String method = tokenizer.nextToken();
+        String path = tokenizer.nextToken();
+        String protocol = tokenizer.nextToken();
+
+        // header 읽기
+        String line;
+        Map<String, String> header = new HashMap<>();
+        while (!(line = br.readLine()).isEmpty()) {
+            String[] parts = line.split(":", 2);
+            String key = parts[0].trim();
+            String value = parts[1].trim();
+            header.put(key, value);
+        }
+
+        // TODO: body 읽기
+        //int contentLength = Integer.parseInt(header.get("Content-Length"));
+
+        return new HttpRequest(method, path, protocol, Map.copyOf(header));
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,17 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,14 +24,26 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            HttpRequest request = HttpRequest.from(br);
+            String method = request.getMethod();
+            String path = request.getPath();
+
+            // TODO: 메서드 별로 분리
+            File file = new File("src/main/resources/static" + path);
             byte[] body = "<h1>Hello World</h1>".getBytes();
+            if (file.canRead()) {
+                body = Files.readAllBytes(file.toPath());
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
