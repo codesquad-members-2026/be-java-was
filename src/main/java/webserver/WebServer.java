c95b103a31366f59aa4b33321c5477179e1d9db8
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-04-09 12:05:17 +0900

    step1 : index.html 응답 받기
    RequestHandler.java
    
    - 배열 객체 생성후 GET 포함한 헤더 첫줄 읽어와 띄어쓰기 기준으로
      쪼개주기
    - 쪼갠 조각중 인덱스 번호 1인 / 을 가져와서 /일 경우 index.html로
      지정해주기
    - index.html에 있는 모든 부분을 바이트로 가져와 바이트 배열 객체에
      저장하기
    
    WebServer.java
    
    - 10개의 쓰레드로 개수를 지정해줘서 구현
    - 가상 쓰레드로 교체하여 요청 받을때마다 가상 쓰레드 하나씩 생성하는
      로직 구현

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,9 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,9 +27,13 @@ public class WebServer {
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
+
+          //  고정 크기 방식 ExecutorService execcutor = Executors.newFixedThreadPool(10)
+
+            ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.execute(new RequestHandler(connection));
             }
         }
     }
