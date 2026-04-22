e81161a28ee4ed5983adc4724557babe4ab79e43
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-04-22 17:46:45 +0900

    step6 : 로그인, 로그아웃, 회원가입
    - POST 요청을 통해 전달된 유저 파라미터 추출 및 User 객체 생성
    - 생성된 유저 정보를 Database에 저장하고 메인 페이지로 리다이렉트 처리
    - GET 방식의 잘못된 요청에 대한 예외 처리 로직 추가
    
    - UserLoginAction: 사용자의 ID/PW 검증 후 세션 생성 및 Set-Cookie 헤더를 통한 SID 전달 로직 구현
    
    - UserLogoutAction: 요청 헤더의 SID 쿠키를 확인하여 SessionManager에서 해당 세션 삭제
    - 응답 헤더에 Max-Age=0 속성을 가진 쿠키를 추가하여 브라우저의 세션 쿠키 즉시 만료 처리

--- src/main/java/action/UserLoginAction.java
+++ src/main/java/action/UserLoginAction.java
@@ -14,36 +14,22 @@ public class UserLoginAction implements Action{
 
     private static final Logger logger = LoggerFactory.getLogger(UserLoginAction.class);
 
-    @Override
     public void execute(HttpRequest request, HttpResponse response) throws IOException {
-        if (!"POST".equals(request.getMethod())) {
-            logger.error("GET 방식으로는 로그인이 되지 않습니다.");
-            response.sendRedirect("/login/index.html");
-            return;
-        }
-
-        // TODO: HTML Form의 input name 속성이 "username"과 "password"가 맞는지 다시 한번 확인해 보세요.
-        String userId = request.getParameter("username");
-        String userPassword = request.getParameter("password");
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
 
         User user = Database.findUserById(userId);
-        SessionManager sessionManager = SessionManager.getInstance();
 
-        if (user != null && userPassword.equals(user.getPassword())) {
-            // TODO: 회원가입 시 비밀번호를 암호화했다면, 여기서도 입력받은 비밀번호를 암호화하여 비교해야 합니다.
-            logger.debug("로그인 성공: {}", userId);
+        if (user != null && user.getPassword().equals(password)) {
+            String sid = SessionManager.getInstance().createSession(user);
 
-            String sid = sessionManager.createSession(user);
+            logger.debug("로그인 성공: 사용자 ID=[{}], 이름=[{}], 세션 ID=[{}]", user.getUserId(), user.getName(), sid);
 
-            // TODO: 보안을 위해 브라우저의 스크립트가 쿠키에 접근하지 못하도록 'HttpOnly' 옵션을 추가하는 것을 고민해 보세요.
             response.addHeader("Set-Cookie", "SID=" + sid + "; Path=/");
             response.sendRedirect("/index.html");
             return;
         }
-
-        // TODO: 로그인 실패 시, 사용자에게 왜 실패했는지(아이디 부재 혹은 비밀번호 불일치)를
-        // 쿼리 스트링이나 세션을 통해 전달할 방법이 있을지 고민해 보세요.
-        logger.debug("로그인 실패: {}", userId);
+        logger.debug("로그인 실패: ID=[{}]", userId);
         response.sendRedirect("/user/login_failed.html");
     }
 }
