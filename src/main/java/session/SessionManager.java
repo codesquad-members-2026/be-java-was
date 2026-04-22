a3955cbdfd65addf0fcf38ce1ed88ebdaaefc8c6
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-04-22 17:48:27 +0900

    feat : 리팩토링 구현
    - SessionManager: UUID를 활용한 고유 세션 ID 생성 및 ConcurrentHashMap을 이용한 세션 저장소 구현

--- src/main/java/session/SessionManager.java
+++ src/main/java/session/SessionManager.java
@@ -25,15 +25,13 @@ public class SessionManager {
     }
 
     public User read(String sessionId) {
-        // TODO: 만약 존재하지 않거나 만료된 세션 ID를 조회했을 때의 예외 처리나 반환값에 대해 고민해 보세요.
+        if (sessionId == null) {
+            return null;
+        }
         return sessions.get(sessionId);
     }
-
     public void delete(String sessionId) {
         sessions.remove(sessionId);
     }
-
-    // TODO (심화): 오래된 세션을 자동으로 정리(Clean up)하는 로직이 필요할까요?
-    // 세션의 '생성 시간'을 기록하고 일정 시간이 지나면 삭제하는 '만료 기능'의 설계를 구상해 보세요.
 
 }
