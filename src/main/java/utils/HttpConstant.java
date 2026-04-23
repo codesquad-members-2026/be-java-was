6c92414255b411dde5e9dc8bc51729500ce9358f
LeeJaeWan <jjjkuul@naver.com>
2026-04-23 21:04:43 +0900

    step-6 구현 완료

--- src/main/java/utils/HttpConstant.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class HttpConstant {
-    private HttpConstant() {}
-
-    public static final String CRLF = "\r\n";
-}
--- src/test/java/webserver/request/HttpRequestTest.java
+++ src/test/java/webserver/request/HttpRequestTest.java
@@ -1,5 +1,6 @@
 package webserver.request;
 
+import core.request.HttpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
